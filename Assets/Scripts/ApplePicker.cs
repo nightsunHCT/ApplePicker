e18e99a499b1cdb456d1d6da8d9a82247080064d
Helena <nightsunHCT@users.noreply.github.com>
2020-02-02 02:24:40 -0500

    Basket Positioning

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject basketPrefab;
+    public int numBasket = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < numBasket; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+        }
     }
 }
--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get the current screen postion of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // The Camera's Z position sets how far to push the mouse into 3D 
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Move the x position of thsi Basket to the x position of the mouse 
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos; 
     }
 }

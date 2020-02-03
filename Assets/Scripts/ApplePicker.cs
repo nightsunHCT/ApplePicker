a23541bc3c1b58e024f1538ebc38a2f378085f0e
Helena <nightsunHCT@users.noreply.github.com>
2020-02-03 02:04:38 -0500

    Destory Basket

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -8,15 +9,18 @@ public class ApplePicker : MonoBehaviour
     public int numBasket = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     void Start()
     {
         for (int i = 0; i < numBasket; i++)
         {
+            basketList = new List<GameObject>();
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
 
@@ -28,5 +32,14 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGO);
         }
+
+        //Destory one of the baskets
+        // get the index of the last Basket in the basketList
+        int basketIndex = basketList.Count - 1;
+        // Get a reference to that Basket gameObject 
+        GameObject tBasketGO = basketList[basketIndex];
+        // remove the Basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 }
